1ae601e67353c1f6005d37ee478162eb9a152ce7
Colasuonno <colasuonno.scuola@gmail.com>
2020-05-15 13:59:34 +0200

    Geo flags

---
+++ colasuonno_wikiresp/models/GeographicModels.py
@@ -0,0 +1,13 @@
+
+def flag_image():
+    return [
+        ["flag_image"],
+        {}, {"P41": "?flag_image"}, {}, {}
+    ]
+
+
+def citizens_count():
+    return [
+        ["citizens_count"],
+        {}, {}, {}, {}
+    ]
--- colasuonno_wikiresp/test.py
+++ colasuonno_wikiresp/test.py
@@ -3,21 +3,23 @@ from statements_query.StatementWikiQuery import StatementWikiQuery
 from datetime import datetime
 from models import PlanetModels as pm
 from models import HumansModels as hm
+from models import GeographicModels as gm
 
 
-
-starting = datetime.now()
 #query = DirectWikiQuery("Aster", "Dante Alighieri")
 #elements = hm.languages_spoken()
 #query.lazy_init(elements[0], elements[1], elements[2], elements[3], elements[4])
 #print(query.pretty_print())
 
-query = StatementWikiQuery("Aster", "Earth")
-elements = pm.radius()
-query.lazy_init(elements[0], elements[1], elements[2], 1)
-print(query.pretty_print())
+query = DirectWikiQuery("Aster", "Great Britain")
 
-end = datetime.now()
-tot = end - starting
-print({"Request elapsed time": str(tot.total_seconds()) + "s"})
+while(True):
+    name = input("Block: ")
+    starting = datetime.now()
+    elements = gm.flag_image()
+    query.lazy_init(elements[0], elements[1], elements[2], elements[3], elements[4])
+    print(query.pretty_print())
+    end = datetime.now()
+    tot = end - starting
+    print({"Request elapsed time": str(tot.total_seconds()) + "s"})
 
